0e6ae86a784ab1d1f95fe016ccbaf9d23204b54d
laurenjanko <62356246+laurenjanko@users.noreply.github.com>
2021-11-15 21:18:18 -0600

    Create Home.js

---
+++ src/pages/Home.js
@@ -0,0 +1,22 @@
+import React from 'react'
+import './Home.css'
+
+const Home = () =>{
+    return (
+        <div>
+            <div className="home section_margin" id="parks">
+                <div className="home-feature-title">
+                    <h1> National Parks</h1>
+                </div>
+                <div className="home-feature-text">
+                    <p>Welcome to a website to show you different national
+                        parks as well as the activites you can find within
+                        the parks!
+                    </p>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Home
