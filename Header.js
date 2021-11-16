0d0b07080b4aa9db2f49268a57f34b0ec5e952be
laurenjanko <62356246+laurenjanko@users.noreply.github.com>
2021-11-15 21:14:20 -0600

    Add files via upload

---
+++ Header.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+const Header = () =>{
+    return(
+        <div className="header section_padding" id='home'>
+            <h1 className="top_text">Welcome!</h1>
+        </div>
+    )
+}
+export default Header;
